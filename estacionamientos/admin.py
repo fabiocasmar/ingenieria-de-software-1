875b8a0aa26229aae5aed0d93ce18e251db52bb8
Fabio Castro <fabiocasmar@gmail.com>
2015-05-28 21:03:11 -0430

    Merge con Billetera

--- estacionamientos/admin.py
+++ estacionamientos/admin.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from estacionamientos.models import Estacionamiento, Reserva, Pago, TarifaMinuto,\
-TarifaHorayFraccion, TarifaHora, Usuario, Billetera, Propietario
+from estacionamientos.models import Estacionamiento, Reserva, Pago, TarifaMinuto, TarifaHorayFraccion, TarifaHora, Usuario, Billetera, Propietario
 
 admin.site.register(Estacionamiento)
 admin.site.register(Propietario)
@@ -12,4 +11,3 @@ admin.site.register(TarifaMinuto)
 admin.site.register(TarifaHorayFraccion)
 admin.site.register(Usuario)
 admin.site.register(Billetera)
-admin.site.register(Propietario)
